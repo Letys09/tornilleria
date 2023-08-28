b742f21b2fc0da13f285fce9cf29ad63457aa526
Lety <leticia@ddsmedia.net>
2023-08-28 17:21:47 -0600

    Sucursales
    CRUD sucursales

---
+++ app/model/sucursal_model.php
@@ -0,0 +1,144 @@
+<?php
+	namespace App\Model;
+	use App\Lib\Response;
+
+class SucursalModel {
+	private $db;
+	private $table = 'sucursal';
+	private $tableDir = 'direccion';
+	private $response;
+	
+	public function __CONSTRUCT($db) {
+		date_default_timezone_set('America/Mexico_City');
+		if(!isset($_SESSION)) { session_start(); }
+		$this->db = $db;
+		$this->response = new Response();
+	}
+
+    public function get($id) {
+		$this->response->result = $this->db
+			->from($this->table)
+            ->select(null)
+            ->select("direccion_id, nombre, telefono, calle, no_ext, no_int, colonia, municipio, estado, codigo_postal")
+            ->innerJoin("$this->tableDir ON $this->tableDir.id = $this->table.direccion_id")
+			->where("$this->table.id", $id)
+			->fetch();
+
+		if($this->response->result) { $this->response->SetResponse(true); }
+		else { $this->response->SetResponse(false, 'No existe el registro '.$id); }
+
+		return $this->response;
+	}
+
+    public function getAllDataTable() {
+		$this->response->result = $this->db
+			->from($this->table)
+			->select(null)
+			->select("$this->table.id, nombre, $this->tableDir.calle, $this->tableDir.no_ext, $this->tableDir.no_int, $this->tableDir.colonia, $this->tableDir.municipio, 
+                    $this->tableDir.estado, $this->tableDir.codigo_postal, telefono")
+			->where("status", 1)
+			->fetchAll(); 
+					
+		return $this->response->SetResponse(true);
+	}
+
+	public function add($data){
+
+		$SQL = $this->db
+			->insertInto($this->table, $data)
+			->execute();
+		
+		$this->response->result = $SQL;
+		if($this->response->result){
+			$this->response->SetResponse(true, 'Sucursal agregada.'); 
+		} else {
+			$this->response->SetResponse(false, 'No se pudo agregar la sucursal.'); 
+		}
+			
+		return $this->response;
+	}
+
+	public function edit($data, $id){
+		try {
+			$this->response->result = $this->db
+				->update($this->table, $data)
+				->where('id', $id)
+				->execute();
+
+				if($this->response->result) { $this->response->SetResponse(true); }
+				else { $this->response->SetResponse(false, 'No se actualizo el registro del usuario '); }
+
+		} catch(\PDOException $ex) {
+			$this->response->errors = $ex;
+			$this->response->SetResponse(false, 'catch: editar sucursal');
+		}
+
+		return $this->response;
+	}
+
+	public function del($id) {
+		try {
+            $data['status'] = 0;
+			$this->response->result = $this->db
+				->update($this->table, $data)
+				->where('id', $id)
+				->execute();
+			if($this->response->result) { $this->response->SetResponse(true); }
+			else { $this->response->SetResponse(false, 'No se eliminó lasucursal'); }
+		} catch(\PDOException $ex) {
+			$this->response->errors = $ex;
+			$this->response->SetResponse(false, "catch: del sucursal");
+		}
+		return $this->response;
+	}
+
+    public function getDir($id) {
+		$this->response->result = $this->db
+			->from($this->tableDir)
+            ->select(null)
+            ->select("calle, no_ext, no_int, colonia, municipio, estado, codigo_postal")
+			->where("id", $id)
+			->fetch();
+
+		if($this->response->result) { $this->response->SetResponse(true); }
+		else { $this->response->SetResponse(false, 'No existe la dirección '.$id); }
+
+		return $this->response;
+	}
+
+    public function addDir($data){
+        		
+		$SQL = $this->db
+			->insertInto($this->tableDir, $data)
+			->execute();
+		
+		$this->response->result = $SQL;
+		if($this->response->result){
+			$this->response->SetResponse(true, 'Dirección agregada.'); 
+		} else {
+			$this->response->SetResponse(false, 'No se pudo agregar la dirección de la sucursal.'); 
+		}
+			
+		return $this->response;
+	}
+
+    public function editDir($data, $id){
+		try {
+			$this->response->result = $this->db
+				->update($this->tableDir, $data)
+				->where('id', $id)
+				->execute();
+
+				if($this->response->result) { $this->response->SetResponse(true); }
+				else { $this->response->SetResponse(false, 'No se actualizo la dirección '.$id); }
+
+		} catch(\PDOException $ex) {
+			$this->response->errors = $ex;
+			$this->response->SetResponse(false, 'catch: editar dirección');
+		}
+
+		return $this->response;
+	}
+	
+}
+?>
---
+++ app/route/sucursal_route.php
@@ -0,0 +1,157 @@
+<?php
+use App\Lib\Auth,
+	App\Lib\Response,
+	App\Lib\MiddlewareToken,
+	App\Middleware\AccesoMiddleware;
+
+	date_default_timezone_set('America/Mexico_City');
+
+	$app->group('/sucursal/', function () use ($app){
+
+        $this->get('get/{id}', function ($req, $res, $args) {
+			return $res->withJson($this->model->sucursal->get($args['id']));
+		});
+
+        $this->get('getAllDataTable', function($req, $res, $args){
+			$sucursales = $this->model->sucursal->getAllDataTable();
+
+			$data = [];
+			if(!isset($_SESSION)) { session_start(); }
+			foreach($sucursales->result as $sucursal) {
+                $int = $sucursal->no_int != '' && $sucursal->no_int != NULL ? ', Int. '.$sucursal->no_int : '';
+                $dir = 'Calle '.$sucursal->calle.' '.$sucursal->no_ext.$int.', Colonia '.$sucursal->colonia.', '.$sucursal->municipio.', '.$sucursal->estado.'. C.P. '.$sucursal->codigo_postal;
+				$data[] = array(
+					"id" => $sucursal->id,
+					"nombre" => $sucursal->nombre,
+					"direccion" => $dir,
+					"telefono" => $sucursal->telefono,
+				);
+			}
+
+			echo json_encode(array(
+				'data' => $data
+			));
+			exit(0);
+            // return $res->withJson($sucursales);
+		});
+
+		$this->post('add/',function($req,$res,$args){
+			$this->model->transaction->iniciaTransaccion();
+            $parsedBody = $req->getParsedBody();
+            $data = [
+                'calle' => $parsedBody['calle'],
+                'no_ext' => $parsedBody['no_ext'],
+                'no_int' => $parsedBody['no_int'],
+                'colonia' => $parsedBody['colonia'],
+                'municipio' => $parsedBody['municipio'],
+                'estado' => $parsedBody['estado'],
+                'codigo_postal' => $parsedBody['codigo_postal']
+            ];                
+            unset($parsedBody['calle'], $parsedBody['no_ext'], $parsedBody['no_int'], $parsedBody['colonia'], $parsedBody['municipio'], $parsedBody['estado'], $parsedBody['codigo_postal']);
+            
+            $direccion = $this->model->sucursal->addDir($data);
+            if($direccion->response){
+                $parsedBody['direccion_id'] = $direccion->result;
+                $sucursal = $this->model->sucursal->add($parsedBody);
+                if($sucursal->response){
+                    $seg_log = $this->model->seg_log->add('Agrega sucursal', 'sucursal', $sucursal->result, 1);
+                    if($seg_log->response){
+                        $sucursal->state = $this->model->transaction->confirmaTransaccion();	
+                        return $res->withJson($sucursal);
+                    }else{
+                        $seg_log->state = $this->model->transaction->confirmaTransaccion();	
+                        return $res->withJson($seg_log->SetResponse(false, 'No se pudo agregar el registro de bitácora'));
+                    }
+                }else{
+                    $sucursal->state = $this->model->transaction->regresaTransaccion();
+                    return $res->withJson($sucursal->SetResponse(false, 'No se pudo agregar la sucursal'));
+                }
+            }else{
+                $direccion->state = $this->model->transaction->regresaTransaccion();
+                return $res->withJson($direccion->SetResponse(false, 'No se pudo agregar la dirección de la sucursal'));
+            }
+		});
+
+		$this->post('edit/{id}', function($req, $res, $args){
+			$this->model->transaction->iniciaTransaccion();
+			$parsedBody = $req->getParsedBody();
+
+			$igual = true; $dirIgual = true; $editSuc = 0;
+			$info = $this->model->sucursal->get($args['id'])->result;
+
+            $data = [
+				'nombre' => $parsedBody['nombre'],
+				'telefono' => $parsedBody['telefono'],
+			];
+			
+			foreach($data as $field => $value) { 
+                if($info->$field != $value) { 
+                    $igual = false; break; 
+				} 
+			}
+			
+			$dir = $this->model->sucursal->getDir($info->direccion_id)->result;
+			$dataDir = [
+				'calle' => $parsedBody['calle'],
+				'no_ext' => $parsedBody['no_ext'],
+				'no_int' => $parsedBody['no_int'],
+				'colonia' => $parsedBody['colonia'],
+				'municipio' => $parsedBody['municipio'],
+				'estado' => $parsedBody['estado'],
+				'codigo_postal' => $parsedBody['codigo_postal'],
+			];
+
+            foreach($dataDir as $field => $value) { 
+				if($dir->$field != $value) { 
+					$dirIgual = false; break; 
+				} 
+			}
+
+			if(!$igual){
+                $editSuc = $this->model->sucursal->edit($data, $args['id']);
+                if($editSuc->response){
+                    $seg_log = $this->model->seg_log->add('Modifica sucursal', 'sucursal', $args['id'], 1);
+                    if(!$seg_log->response){
+                        $seg_log->state = $this->model->transaction->regresaTransaccion();
+                    }
+                }else{
+                    $editSuc->state = $this->model->transaction->regresaTransaccion();
+                }
+			}
+            if(!$dirIgual){
+                $editSuc = $this->model->sucursal->editDir($dataDir, $args['id']);
+                if($editSuc->response){
+                        $seg_log = $this->model->seg_log->add('Modifica dirección', 'dirección', $info->direccion_id, 1);
+                        if(!$seg_log->response){
+                            $seg_log->state = $this->model->transaction->regresaTransaccion();
+                        }
+                }else{
+                    $editSuc->state = $this->model->transaction->regresaTransaccion();
+                }
+            }
+
+            $this->model->transaction->confirmaTransaccion();
+			return $res->withJson($editSuc);
+		});
+
+        $this->post('del/{id}', function($req, $res, $args){
+            $this->model->transaction->iniciaTransaccion();
+            $update = $this->model->sucursal->del($args['id']);
+            if($update->response){
+                $seg_log = $this->model->seg_log->add('Elimina sucursal', 'sucursal', $args['id'], 1);
+                if($seg_log->response){
+                    $update->state = $this->model->transaction->confirmaTransaccion();	
+                    return $res->withJson($update);
+                }else{
+                    $seg_log->state = $this->model->transaction->regresaTransaccion();	
+                    return $res->withJson($sucursal->SetResponse(false, 'No se pudo agregar el registro de bitácora'));
+                }
+            }else{
+                $update->state = $this->model->transaction->regresaTransaccion();	
+                return $res->withJson($update->SetResponse(false, 'No se pudo eliminar la sucursal'));
+            }
+        });
+
+	});
+
+?>
